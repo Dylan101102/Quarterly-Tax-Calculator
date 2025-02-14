703e4d132719d4281e5fb643bdd20e00046397fd
Dylan Roach <dylan.roach@flhealth.gov>
2025-02-14 15:54:20 -0500

    Make formula to calculate how much of quarterly income is taxable

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 # Simple program to calculate quarterly taxes for making deliveries as an independent contractor.
+# By no means is this 100% perfect, but does do a decent job at least of estimating quarterly taxes.
+# For example, income is not taxed at a single tax rate. For simplicity, it will be here. Lowering tax liability come April is the main goal here.
+# For testing purposes, will be using 2024 rates to test if it matches what I paid quarterly last year. Will eventually switch over to 2025 numbers.
 
-estimated_income = float(input("How much do you expect your total income to be this year? You could look at your most recent tax return (Page 1, line 9): $ "))
-filing_status = input("What is your filing status? Press 1 for 'Single', 2 for 'Married Filing Jointly', 3 for 'Married Filing Separately', or 4 for 'Head of Household': ")
+
+def taxable_income(income, miles):
+    """
+    This function determines how much of your quarterly income is taxable.
+    """
+    deduction = miles * 0.67 # 2024 standard mileage rate.
+    tax_income = income - deduction
+    return tax_income
+    
+
+quarterly_income = float(input("From your side jobs, how much did you make for the previous quarter? $"))
+miles_driven = float(input("How many miles did you drive for the previous quarter? "))
+
+quar_tax_income = taxable_income(quarterly_income, miles_driven)
+print(f"Your taxable income for the previous quarter is ${quar_tax_income}")
+
+
+"""
+anticip_yearly_income = float(input("How much do you expect to make this year from all income sources? This will determine your tax rate: $"))
+marital_status = input("To also determine your tax rate, you will need to input your filing status. Press 1 if you are 'Single'," 
+                        " 2 if you are 'Married Filing Jointly', 3 if you are 'Married, Filing Separately', or 4 if you are 'Head of Household': ")
+"""
+
