9cf0df46367cf7050bdbe61521163767b0a044f6
Dylan Roach <dylan.roach@flhealth.gov>
2025-02-18 14:58:45 -0500

    Finish first iteration of program

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # For example, income is not taxed at a single tax rate. For simplicity, it will be here. Lowering tax liability come April is the main goal here.
 # This program also assumes the taxable income between the current and previous year do not differ too much, but again, this was done for simplicity.
 # For testing purposes, will be using 2024 rates to test if it matches what I paid quarterly last year. Will eventually switch over to 2025 numbers.
+# This program also assumes you live in a state with no state income tax, like Florida.
 
 
 def taxable_income(income, miles):
@@ -12,6 +13,7 @@ def taxable_income(income, miles):
     deduction = miles * 0.67 # 2024 standard mileage rate.
     tax_income = round(income - deduction) # Tax software does a lot of rounding, so that is being done here.
     return tax_income
+    
 
 def fed_tax_rate (yearly_income, status):
     """
@@ -79,7 +81,16 @@ def fed_tax_rate (yearly_income, status):
             else:
                 tax_rate = 0.37
     return tax_rate
-        
+
+
+def pay_irs(rate, quarterly_income):
+    """
+    This function determines how much you will pay to the IRS quarterly with the given information.
+    """
+    fed_income_tax = rate * quarterly_income
+    self_employment_tax = (quarterly_income * 0.9235) * 0.153
+    total_taxes = fed_income_tax + self_employment_tax
+    return round(total_taxes)
 
 
 quarterly_income = float(input("From your side jobs, how much did you make for the previous quarter? $"))
@@ -91,4 +102,5 @@ anticip_yearly_income = float(input("What was your taxable income last year? Thi
 marital_status = input("To also determine your tax rate, you will need to input your filing status. Press 1 if you are 'Single'," 
                         " 2 if you are 'Married Filing Jointly', \n3 if you are 'Married, Filing Separately', or 4 if you are 'Head of Household': ")
 tax_rate = fed_tax_rate(anticip_yearly_income, marital_status)
-# print(tax_rate)
+irs_payment = pay_irs(tax_rate, quar_tax_income)
+print(f"Please owe the IRS ${irs_payment}.")
