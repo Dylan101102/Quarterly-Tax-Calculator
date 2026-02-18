3eb9b891d64bd7a74d31b5ed7a1155e4f9a994b7
Dylan Roach <dylan.roach@flhealth.gov>
2026-02-18 15:36:08 -0500

    "Catch-up" testing "main.py" file to main "main.py" file

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 # By no means is this 100% perfect, but does do a decent job at least of estimating quarterly taxes.
 # For example, income is not taxed at a single tax rate. For simplicity, it will be here. Lowering tax liability come April is the main goal here.
 # This program also assumes the taxable income between the current and previous year do not differ too much, but again, this was done for simplicity.
-# For testing purposes, will be using 2024 rates to test if it matches what I paid quarterly last year. Will eventually switch over to 2025 numbers.
 # This program also assumes you live in a state with no state income tax, like Florida.
 
 import functions
 
 quarterly_income = float(input("From your side jobs, how much did you make for the previous quarter? $"))
+tips = float(input("Of that amount, how much was from tips? $"))
 miles_driven = float(input("How many miles did you drive for the previous quarter? "))
-quar_tax_income = functions.taxable_income(quarterly_income, miles_driven)
-# print(f"Your taxable income for the previous quarter is ${quar_tax_income}")
+quar_tax_income = functions.taxable_income(quarterly_income, tips, miles_driven)
 
-anticip_yearly_income = float(input("What was your taxable income last year? This will determine your tax rate: $"))
+previous_year_taxable_income = float(input("What was your taxable income last year? This will determine your tax rate: $"))
+# previous_year_taxable_income = WILL BE FROM 2025 TAXES FILED IN 2026. THIS WILL BE SET TO A CONSTANT.
 marital_status = input("To also determine your tax rate, you will need to input your filing status. Press 1 if you are 'Single'," 
                         " 2 if you are 'Married Filing Jointly', \n3 if you are 'Married, Filing Separately', or 4 if you are 'Head of Household': ")
-tax_rate = functions.fed_tax_rate(anticip_yearly_income, marital_status)
+tax_rate = functions.fed_tax_rate(previous_year_taxable_income, marital_status)
 irs_payment = functions.pay_irs(tax_rate, quar_tax_income)
 print(f"Please owe the IRS ${irs_payment}.")
