9832841ca091f6f3529e78eaa2d7ab2e3ae8745c
Dylan Roach <dylan.roach@flhealth.gov>
2026-02-18 15:37:31 -0500

    "Catch-up" testing "functions.py" file to main "functions.py" file

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
-def taxable_income(income, miles):
+def taxable_income(income, quarterly_tips, miles):
     """
     This function determines how much of your quarterly income is taxable.
     """
-    deduction = miles * 0.70 # 2025 standard mileage rate.
+    standard_mileage_deduction = 0.725  # For 2026 tax year.
+    deduction = miles * standard_mileage_deduction
     tax_income = round(income - deduction) # Tax software does a lot of rounding, so that is being done here.
     return tax_income
 
@@ -13,62 +14,62 @@ def fed_tax_rate (yearly_income, status):
     """
     match status:
         case "1": # Single status filer.
-            if yearly_income <= 11925:
+            if yearly_income <= 12400:
                 tax_rate = 0.10
-            elif yearly_income >= 11926 and yearly_income <= 48475:
+            elif yearly_income >= 12401 and yearly_income <= 50400:
                 tax_rate = 0.12
-            elif yearly_income >= 48476 and yearly_income <= 103350:
+            elif yearly_income >= 50401 and yearly_income <= 105700:
                 tax_rate = 0.22
-            elif yearly_income >= 103351 and yearly_income <= 197300:
+            elif yearly_income >= 105701 and yearly_income <= 201775:
                 tax_rate = 0.24
-            elif yearly_income >= 197301 and yearly_income <= 250525:
+            elif yearly_income >= 201776 and yearly_income <= 256225:
                 tax_rate = 0.32
-            elif yearly_income >= 250526 and yearly_income <= 626350:
+            elif yearly_income >= 256226 and yearly_income <= 640600:
                 tax_rate = 0.35
             else:
                 tax_rate = 0.37
         case "2": # Married Filing Jointly
-            if yearly_income <= 23850:
+            if yearly_income <= 24800:
                 tax_rate = 0.10
-            elif yearly_income >= 23851 and yearly_income <= 96950:
+            elif yearly_income >= 24801 and yearly_income <= 100800:
                 tax_rate = 0.12
-            elif yearly_income >= 96951 and yearly_income <= 206700:
+            elif yearly_income >= 100801 and yearly_income <= 211400:
                 tax_rate = 0.22
-            elif yearly_income >= 206701 and yearly_income <= 394600:
+            elif yearly_income >= 211401 and yearly_income <= 403550:
                 tax_rate = 0.24
-            elif yearly_income >= 394601 and yearly_income <= 501050:
+            elif yearly_income >= 403551 and yearly_income <= 512450:
                 tax_rate = 0.32
-            elif yearly_income >= 501051 and yearly_income <= 751600:
+            elif yearly_income >= 512451 and yearly_income <= 768700:
                 tax_rate = 0.35
             else:
                 tax_rate = 0.37
         case "3": # Married Filing Separately
-            if yearly_income <= 11925:
+            if yearly_income <= 12400:
                 tax_rate = 0.10
-            elif yearly_income >= 11926 and yearly_income <= 48475:
+            elif yearly_income >= 12401 and yearly_income <= 50400:
                 tax_rate = 0.12
-            elif yearly_income >= 48476 and yearly_income <= 103350:
+            elif yearly_income >= 50401 and yearly_income <= 105700:
                 tax_rate = 0.22
-            elif yearly_income >= 103351 and yearly_income <= 197300:
+            elif yearly_income >= 105701 and yearly_income <= 201775:
                 tax_rate = 0.24
-            elif yearly_income >= 197301 and yearly_income <= 250525:
+            elif yearly_income >= 201776 and yearly_income <= 256225:
                 tax_rate = 0.32
-            elif yearly_income >= 250526 and yearly_income <= 375800:
+            elif yearly_income >= 256226 and yearly_income <= 384350:
                 tax_rate = 0.35
             else:
                 tax_rate = 0.37
         case "4": # Head of Household
-            if yearly_income <= 17000:
+            if yearly_income <= 17700:
                 tax_rate = 0.10
-            elif yearly_income >= 17001 and yearly_income <= 64850:
+            elif yearly_income >= 17701 and yearly_income <= 67450:
                 tax_rate = 0.12
-            elif yearly_income >= 64851 and yearly_income <= 103350:
+            elif yearly_income >= 67451 and yearly_income <= 105700:
                 tax_rate = 0.22
-            elif yearly_income >= 103351 and yearly_income <= 197300:
+            elif yearly_income >= 105701 and yearly_income <= 201750:
                 tax_rate = 0.24
-            elif yearly_income >= 197301 and yearly_income <= 250500:
+            elif yearly_income >= 201751 and yearly_income <= 256200:
                 tax_rate = 0.32
-            elif yearly_income >= 250501 and yearly_income <= 626350:
+            elif yearly_income >= 256201 and yearly_income <= 640600:
                 tax_rate = 0.35
             else:
                 tax_rate = 0.37
@@ -79,7 +80,13 @@ def pay_irs(rate, quarterly_income):
     """
     This function determines how much you will pay to the IRS quarterly with the given information.
     """
+    social_security_tax_rate = 0.124
+    medicare_tax_rate = 0.029
+    self_employment_tax_rate = social_security_tax_rate + medicare_tax_rate
+    earnings_for_self_employment_tax = quarterly_income * 0.9235
+    self_employment_tax = earnings_for_self_employment_tax * self_employment_tax_rate
+
     fed_income_tax = rate * quarterly_income
-    self_employment_tax = (quarterly_income * 0.9235) * 0.153
     total_taxes = fed_income_tax + self_employment_tax
+
     return round(total_taxes)
